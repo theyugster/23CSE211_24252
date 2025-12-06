ef3951007b91b34256398ae3b3287fa6df92951a
theyugster <Theyugster@proton.me>
2025-12-06 17:22:00 +0530

    fixed minor bug in factorial.c

--- Lab1/factorial.c
+++ Lab1/factorial.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 int factorial(int num){
-    if(num==2){
-        return 2;
+    if(num==0){
+        return 1;
     }
     return num*(factorial(num-1));
 }
@@ -19,4 +19,5 @@ This is done using recursion with a base case that returns 2(becuse 2! is 2).
 Each time the function calls it self, it decrements num by 1, so the base case
 is always reached. 
 After that it will perform the multiplication, which then gives the answer.
+
 */
