5d5596041d752724bbafc1edddb4e4fcd4d16fde
theyugster <Theyugster@proton.me>
2025-12-06 17:22:26 +0530

    Update factorial.c reasoning

--- Lab1/factorial.c
+++ Lab1/factorial.c
@@ -15,9 +15,10 @@ int main(){
 Time Complexity:O(n) linear time beacuse only one function call
 Space Complexity:O(n) linear space beacuse of the recursion calling
 
-This is done using recursion with a base case that returns 2(becuse 2! is 2).
+This is done using recursion with a base case that returns 1(becuse 0! is 1).
 Each time the function calls it self, it decrements num by 1, so the base case
 is always reached. 
 After that it will perform the multiplication, which then gives the answer.
 
 */
+
